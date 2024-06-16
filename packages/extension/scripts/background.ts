1dd994c70e61b696644cb5d9c41f9bda8e044b32
simochee <lollipop@simochee.net>
2024-06-16 21:56:57 +0900

    feat: before unload

--- packages/extension/scripts/background.ts
+++ packages/extension/scripts/background.ts
@@ -20,8 +20,26 @@ browser.action.onClicked.addListener(async (tab) => {
 
 	if (tabId) {
 		browser.storage.local.remove("tabId");
-	} else if (tab.url?.includes("meet.google.com")) {
+	} else if (tab.url?.includes("meet.google.com") && tab.id != null) {
 		browser.storage.local.set({ tabId: tab.id });
+
+		browser.scripting.executeScript({
+			target: { tabId: tab.id },
+			func: () => {
+				window.addEventListener("beforeunload", () => {
+					// @ts-ignore
+					chrome.runtime.sendMessage({ type: "disconnect" });
+				});
+			},
+		});
+	}
+});
+
+browser.tabs.onRemoved.addListener(async (tabId) => {
+	const activeTabId = await getActiveTabId();
+
+	if (tabId === activeTabId) {
+		browser.storage.local.remove("tabId");
 	}
 });
 
@@ -32,13 +50,22 @@ browser.runtime.onInstalled.addListener(() => {
 browser.runtime.onMessage.addListener(async (message, sender) => {
 	const tabId = await getActiveTabId();
 
+	console.log("message", message, tabId, sender.tab?.id);
+
 	if (tabId !== sender.tab?.id) return;
 
-	fetch("http://localhost:9043", {
-		method: "post",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(message),
-	});
+	switch (message.type) {
+		case "disconnect":
+			browser.storage.local.remove("tabId");
+			break;
+		default:
+			fetch("http://localhost:9043", {
+				method: "post",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(message),
+			});
+			break;
+	}
 });
