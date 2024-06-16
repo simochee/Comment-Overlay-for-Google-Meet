7f411740fec6a0ccf0b9d3f46298dceafc6cbc55
simochee <lollipop@simochee.net>
2024-06-16 22:10:07 +0900

    refactor: remove old file

--- packages/extension/src/background/observer.ts
+++
@@ -1,7 +0,0 @@
-import browser from "webextension-polyfill";
-
-export const observer = async () => {
-	console.log("observer");
-	const browser = await import("webextension-polyfill");
-	console.log("hello world");
-};
