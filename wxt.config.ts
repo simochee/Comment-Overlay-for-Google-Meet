372649e38eab3c4326a27efc80aae07ed9392684
simochee <lollipop@simochee.net>
2024-08-06 17:13:58 +0900

    chore: shrink permission

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "wxt";
 export default defineConfig({
 	manifest: {
 		name: "Comment Overlay for Google Meet",
-		permissions: ["storage", "notifications"],
+		permissions: ["storage"],
 	},
 	modules: ["@wxt-dev/module-react"],
 	vite: () => ({
